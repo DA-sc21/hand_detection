534088b59c3726959015ea5a1359c10447aead38
KoYeJoon <yejoon08@ajou.ac.kr>
2021-11-13 22:29:43 +0900

    yj data로만 test 한 결과 산출

--- dataset/raw_custom/video2image.py
+++ dataset/raw_custom/video2image.py
@@ -3,18 +3,18 @@ import argparse
 import glob
 
 def video2image(opt):
-    # video_paths = glob.glob(opt.video_dir+"/*.mov")
-    # print(video_paths)
-    video_paths = ['/Users/yejoonko/git/Project/Capstone/hand_detection/dataset/raw_custom/videos/view_front_person_1_writing.mov']
-    count = 0
+    video_paths = glob.glob(opt.video_dir+"/*.mov")
+    print(video_paths)
+    # video_paths = ['/Users/yejoonko/git/Project/Capstone/hand_detection/dataset/raw_custom/videos/view_front_person_1_writing.mov']
+    count = int(opt.start_num)
     for vp in video_paths:
         video_name = vp.split("/")[-1][:-4]
         vidcap = cv2.VideoCapture(vp)
         success,image = vidcap.read()
         new_path = '../custom/images/'
         while success:
-            cv2.resize(image,(1280,720))
             if count % 15 == 0:
+                image = cv2.resize(image,(1280,720))
                 cv2.imwrite(new_path+video_name+"_%04d.jpg" % (count//15), image)     # save frame as JPEG file
             success,image = vidcap.read()
             # print('Read a new frame: ', success)
@@ -28,6 +28,7 @@ def video2image(opt):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--video_dir', type=str, help='path to video directory')
+    parser.add_argument('--start_num',type=str,help='start_num')
     opt = parser.parse_args()
 
     video2image(opt)
